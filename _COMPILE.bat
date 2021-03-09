104e44585fe5d6f9a18a4670eb88b9b5c3786c80
Aaron Landwehr <snaphat@gmail.com>
2021-03-09 03:15:42 -0500

    Add Install and uninstall scripts. Zip files during compilation.

---
+++ _COMPILE.bat
@@ -0,0 +1,6 @@
+del virtual_desktop_enhancer.zip virtual_desktop_enhancer.exe
+Ahk2Exe.exe /in "virtual_desktop_enhancer.ahk" /icon "app.ico" /compress 2
+:loop
+@tasklist /fi "imagename eq Ahk2Exe.exe" |find ":" > nul
+@if errorlevel 1 goto loop > nul
+zip -r  virtual_desktop_enhancer.zip virtual_desktop_enhancer.exe _INSTALL.bat _UNINSTALL.bat README.md
---
+++ _UNINSTALL.bat
@@ -0,0 +1,1 @@
+powershell "del '%userprofile%\Start Menu\Programs\Startup\virtual_desktop_enhancer.lnk'"
