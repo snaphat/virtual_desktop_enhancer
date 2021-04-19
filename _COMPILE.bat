3e7d82115f31f84dd8717faaa862571a972f949d
Aaron Landwehr <snaphat@gmail.com>
2021-04-18 20:13:12 -0400

    - Add default config file. - Add default config file to compilation zip. - Bump version. - Add stuff to .gitignore.

--- _COMPILE.bat
+++ _COMPILE.bat
@@ -3,4 +3,4 @@ Ahk2Exe.exe /in "virtual_desktop_enhancer.ahk" /icon "app.ico" /compress 2
 :loop
 @tasklist /fi "imagename eq Ahk2Exe.exe" |find ":" > nul
 @if errorlevel 1 goto loop > nul
-zip -r  virtual_desktop_enhancer.zip virtual_desktop_enhancer.exe _INSTALL.bat _UNINSTALL.bat README.md VERSION
+zip -r  virtual_desktop_enhancer.zip virtual_desktop_enhancer.exe config.default.ini _INSTALL.bat _UNINSTALL.bat README.md VERSION
